ca63277933988a736f510fd8962251d0432a4c57
Sherry <aleesh05@gmail.com>
2024-09-08 00:49:50 +0100

    first app

--- emp_management/settings.py
+++ emp_management/settings.py
@@ -138,10 +138,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'emp_management/static')
-]
+#STATICFILES_DIRS = [
+   # os.path.join(BASE_DIR, 'emp_management/static')
+#]
 #STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- emp_management/urls.py
+++ emp_management/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
